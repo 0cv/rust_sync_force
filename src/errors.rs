ebc3d7a531df9e3a73ed670ad3c9f3b9a8a9e50a
Christophe Vidal <cvidal@mavens.com>
2023-04-24 20:19:21 +0200

    better error

--- src/errors.rs
+++ src/errors.rs
@@ -17,27 +17,30 @@ pub enum Error {
     IOError(#[from] ::std::io::Error),
 }
 
-
 impl From<ureq::Error> for Error {
     fn from(e: ureq::Error) -> Self {
         match e {
             ureq::Error::Status(status, response) => {
-                println!("ERROR=>>> {:?}", response.into_string());
+                let url = response.get_url().to_string();
+                let response_string = format!("{:?}", response.into_string());
+                let error_response = ErrorResponse {
+                    message: response_string,
+                    error_code: "".to_string(),
+                    fields: None,
+                };
                 return Error::SfdcError {
-                    status: status,
-                    url: "url".into(),//response.get_url().to_string(),
-                    sfdc_errors: Some(vec!()),
+                    status,
+                    url,
+                    sfdc_errors: Some(vec![error_response]),
                     transport_error: None,
-                }
+                };
             }
-            ureq::Error::Transport(transport) => {
-                Error::SfdcError {
-                    status: 0,
-                    url: transport.url().unwrap().to_string(),
-                    sfdc_errors: None,
-                    transport_error: Some(transport.to_string()),
-                }
-            }
+            ureq::Error::Transport(transport) => Error::SfdcError {
+                status: 0,
+                url: transport.url().unwrap().to_string(),
+                sfdc_errors: None,
+                transport_error: Some(transport.to_string()),
+            },
         }
     }
 }
